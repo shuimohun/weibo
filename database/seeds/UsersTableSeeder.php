1ceafa400464beeba5956f89d45cce6e186e9533
shuimohun <8733517@qq.com>
2020-08-14 06:50:06 +0000

    查看用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->times(50)->create();
+        $user = User::find(1);
+        $user->name = 'Shuimohun';
+        $user->email = '8733517@qq.com';
+        $user->save();
+    }
+}
